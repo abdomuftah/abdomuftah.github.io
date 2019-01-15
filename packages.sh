4cc4493df87f351161f6cdcf4cf5518db35be1d7
abdomuftah <abdomuftah@hotmail.com>
2019-01-15 14:27:06 +0200

    upluad Projects

---
+++ packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dpkg-deb -bZgzip projects/appsyncunified debs
+dpkg-deb -bZgzip projects/cracktool3 debs
+dpkg-deb -bZgzip projects/exkey debs
+dpkg-deb -bZgzip projects/inapp debs
+dpkg-deb -bZgzip projects/intelix debs
+dpkg-deb -bZgzip projects/makecydiabuyagain debs
+dpkg-deb -bZgzip projects/midevice debs
+dpkg-deb -bZgzip projects/nostash debs
+dpkg-deb -bZgzip projects/powerselector11 debs
+dpkg-deb -bZgzip projects/semirestore11 debs
+dpkg-deb -bZgzip projects/uasharedtools debs
